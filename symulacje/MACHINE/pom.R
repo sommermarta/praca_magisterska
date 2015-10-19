7d9a6b5bafd4d847f5e5d65bbaa1a72e5a33ab32
Marta <sommerm@student.mini.pw.edu.pl>
2015-10-19 15:27:34 +0200

    machine

---
+++ symulacje/MACHINE/pom.R
@@ -0,0 +1,133 @@
+# http://www.dcc.fc.up.pt/~ltorgo/Regression/DataSets.html
+
+vus <- function(prawdziwe_klasy, estymacja_porzadku){
+    
+    stopifnot(length(prawdziwe_klasy) == length(estymacja_porzadku))
+    
+    o <- order(estymacja_porzadku)
+    posort_dane <- prawdziwe_klasy[o]
+    
+    jakie <- as.numeric(names(table(posort_dane)))
+    mianownik <- prod(table(posort_dane))
+    
+    m <- matrix(0, ncol=length(posort_dane), nrow=length(jakie))
+    
+    ile <- 0
+    for(i in 1:ncol(m)){
+        if(posort_dane[i]==jakie[1]){
+            ile <- ile+1
+        } 
+        m[1,i] <- ile
+    }
+    for(i in 2:nrow(m)){
+        ile <- 0
+        for(j in 1:ncol(m)){
+            if(posort_dane[j]==jakie[i]){
+                if(j==1) ile <- 1 else ile <- m[i, j-1] + m[i-1, j]    
+            }
+            m[i, j] <- ile
+        }
+    }
+    
+    vus <- 100*m[nrow(m), ncol(m)]/mianownik
+    return(vus)
+    
+}
+
+library("MASS")
+library("stringi")
+library("rms")
+library("dplyr")
+library("arules")
+
+# machine <- read.table("C:\\Users\\marsom\\Desktop\\machine.txt",
+#                       sep=",")
+# names(machine)[ncol(machine)] <- "y"
+# 
+# lista_rownoliczne <- vector("list", 4)
+# lista_przedzialy <- vector("list", 4)
+# k <- 1
+# for(i in c(3, 5, 7, 10)){
+#     d <- discretize(machine$y, categories=i, labels=1:i, method="frequency")
+#     dd <- discretize(machine$y, categories=i, labels=1:i, method="cluster")
+#     m <- machine
+#     m$y <- d
+#     lista_rownoliczne[[k]] <- m
+#     m$y <- dd
+#     lista_przedzialy[[k]] <- m
+#     
+#     k <- k+1
+# }
+# 
+# save(lista_rownoliczne, lista_przedzialy, s, 
+#      file="C:\\Users\\marsom\\Desktop\\lista_danych_machine.RData")
+
+load("C:\\Users\\marsom\\Desktop\\lista_danych_machine.RData")
+
+# zbior testowy i treningowy
+
+# set.seed(1234567)
+# s <- sample(1:nrow(machine), floor(0.8*nrow(machine)))
+
+# rownoliczne
+
+tabela_rownoliczne <- matrix(ncol=3, nrow=4)
+for(i in 1:length(lista_rownoliczne)){
+    m <- lrm(y~., data=lista_rownoliczne[[i]][s, ])
+    m_pred_prob <- predict(m, lista_rownoliczne[[i]][-s, ], type="fitted.ind")
+    m_pred_class <- max.col(m_pred_prob)
+    m_pred_f <- predict(m, lista_rownoliczne[[i]][-s, ], type="lp")
+    m_praw_class <- lista_rownoliczne[[i]][-s, ]$y   
+    
+    est_vus <- vus(prawdziwe_klasy=m_praw_class, estymacja_porzadku=m_pred_f)
+    proc_poprawnosci <- sum(m_pred_class == m_praw_class)*100/length(m_praw_class)
+    abss <- sum(abs(as.numeric(as.character(m_praw_class))-m_pred_class))/length(m_pred_class)
+
+    tabela_rownoliczne[i, ] <- c(est_vus, proc_poprawnosci, abss)  
+}
+
+tabela_rownoliczne <- as.data.frame(tabela_rownoliczne)
+colnames(tabela_rownoliczne) <- c("vus", "ppk", "abs")
+rownames(tabela_rownoliczne) <- c("3", "5", "7", "10")
+
+# przedzialy
+
+tabela_przedzialy <- matrix(ncol=3, nrow=4)
+for(i in 1:length(lista_przedzialy)){
+    m <- lrm(as.factor(y)~., data=lista_przedzialy[[i]][s,])
+    m_pred_prob <- predict(m, lista_przedzialy[[i]][-s, ], type="fitted.ind")
+    m_pred_class <- max.col(m_pred_prob)
+    m_pred_f <- predict(m, lista_przedzialy[[i]][-s, ], type="lp")
+    m_praw_class <- lista_przedzialy[[i]][-s, ]$y   
+    
+    est_vus <- vus(prawdziwe_klasy=m_praw_class, estymacja_porzadku=m_pred_f)
+    proc_poprawnosci <- sum(m_pred_class == m_praw_class)*100/length(m_praw_class)
+    abss <- sum(abs(as.numeric(as.character(m_praw_class))-m_pred_class))/length(m_pred_class)
+    
+    tabela_przedzialy[i, ] <- c(est_vus, proc_poprawnosci, abss)  
+}
+
+tabela_przedzialy <- as.data.frame(tabela_przedzialy)
+colnames(tabela_przedzialy) <- c("vus", "ppk", "abs")
+rownames(tabela_przedzialy) <- c("3", "5", "7", "10")
+
+cbind(rodzaj_klastra="metoda_k_srednich", 
+      ile_poziomow_zmiennej_odpowiedzi=c(3, 5, 7, 10),
+      tabela_przedzialy) -> jeden
+
+cbind(rodzaj_klastra="rownoliczne_klastry", 
+      ile_poziomow_zmiennej_odpowiedzi=c(3, 5, 7, 10),
+      tabela_rownoliczne) -> dwa
+
+rbind(jeden, dwa) -> pom
+
+write.table(pom, "C:\\Users\\marsom\\Desktop\\wnioski\\pom.txt", col.names=TRUE, 
+            row.names=FALSE, quote=FALSE, sep="\t")
+
+
+
+
+
+
+
+
---
+++ symulacje/MACHINE/sa.R
@@ -0,0 +1,136 @@
+library("MASS")
+library("stringi")
+library("rms")
+library("dplyr")
+library("arules")
+library("rpart")
+
+vus <- function(prawdziwe_klasy, estymacja_porzadku){
+    
+    stopifnot(length(prawdziwe_klasy) == length(estymacja_porzadku))
+    
+    o <- order(estymacja_porzadku)
+    posort_dane <- prawdziwe_klasy[o]
+    
+    jakie <- as.numeric(names(table(posort_dane)))
+    mianownik <- prod(table(posort_dane))
+    
+    m <- matrix(0, ncol=length(posort_dane), nrow=length(jakie))
+    
+    ile <- 0
+    for(i in 1:ncol(m)){
+        if(posort_dane[i]==jakie[1]){
+            ile <- ile+1
+        } 
+        m[1,i] <- ile
+    }
+    for(i in 2:nrow(m)){
+        ile <- 0
+        for(j in 1:ncol(m)){
+            if(posort_dane[j]==jakie[i]){
+                if(j==1) ile <- 1 else ile <- m[i, j-1] + m[i-1, j]    
+            }
+            m[i, j] <- ile
+        }
+    }
+    
+    vus <- 100*m[nrow(m), ncol(m)]/mianownik
+    return(vus)
+    
+}
+
+###################
+
+load("C:\\Users\\marsom\\Desktop\\lista_danych_machine.RData")
+
+######################
+
+sa <- function(dane_treningowe, dane_testowe){
+
+    ile <- length(levels(dane_treningowe$y))
+    lista_zbiorow <- vector("list", ile-1)
+    
+    for(i in 1:(ile-1)){
+        wektor <- numeric(nrow(dane_treningowe))
+        tabelka <- dane_treningowe
+        wektor[which(as.numeric(as.character(dane_treningowe$y))>i)] <- 1 
+        tabelka$y <- as.factor(wektor)
+        lista_zbiorow[[i]] <- tabelka
+    }
+    
+    lista_modeli <- vector("list", ile-1)
+    lista_klas <- vector("list", ile-1)
+    lista_prawd <- vector("list", ile-1)
+    lista_pstw <- vector("list", ile-1)
+    
+    for(j in 1:length(lista_zbiorow)){
+        lista_modeli[[j]] <- rpart(y~., data=lista_zbiorow[[j]])
+        lista_klas[[j]] <- predict(lista_modeli[[j]], newdata=dane_testowe, type="class")
+        lista_prawd[[j]] <- predict(lista_modeli[[j]], newdata=dane_testowe, type="prob")[,2]
+    }
+    
+    lista_pstw[[1]] <- 1 - lista_prawd[[1]] 
+    lista_pstw[[ile]] <- lista_prawd[[ile-1]]
+    for(i in 2:length(lista_zbiorow)){
+        lista_pstw[[i]] <- lista_prawd[[i-1]] - lista_prawd[[i]]    
+    }
+    
+    tabela <- matrix(lista_pstw[[1]])
+    for(i in 2:ile){
+        tabela <- cbind(tabela, lista_pstw[[i]])
+    }
+    
+    estym_klasa <- max.col(tabela)
+    praw_klasa <- dane_testowe$y
+    f <- apply(tabela, 1, max)+estym_klasa
+    
+    ppk <- 100*sum(estym_klasa==praw_klasa)/length(estym_klasa)
+    est_vus <- vus(prawdziwe_klasy=praw_klasa, estymacja_porzadku=f)
+    abss <- sum(abs(estym_klasa-as.numeric(as.character(praw_klasa))))/length(estym_klasa)
+    
+    c(est_vus, ppk, abss)    
+    
+}
+
+######################
+
+# dane_treningowe <- lista_przedzialy[[1]][s, ]
+# dane_testowe <- lista_przedzialy[[1]][-s, ] 
+
+tabela_rownoliczne <- matrix(ncol=3, nrow=4)
+for(i in 1:length(lista_rownoliczne)){
+    tabela_rownoliczne[i, ] <- sa(lista_rownoliczne[[i]][s, ], lista_rownoliczne[[i]][-s, ])  
+}
+
+tabela_rownoliczne <- as.data.frame(tabela_rownoliczne)
+colnames(tabela_rownoliczne) <- c("vus", "ppk", "abs")
+rownames(tabela_rownoliczne) <- c("3", "5", "7", "10")
+
+# przedzialy
+
+tabela_przedzialy <- matrix(ncol=3, nrow=4)
+for(i in 1:length(lista_rownoliczne)){
+    tabela_przedzialy[i, ] <- sa(lista_przedzialy[[i]][s, ], lista_przedzialy[[i]][-s, ])  
+}
+
+tabela_przedzialy <- as.data.frame(tabela_przedzialy)
+colnames(tabela_przedzialy) <- c("vus", "ppk", "abs")
+rownames(tabela_przedzialy) <- c("3", "5", "7", "10")
+
+cbind(rodzaj_klastra="metoda_k_srednich", 
+      ile_poziomow_zmiennej_odpowiedzi=c(3, 5, 7, 10),
+      tabela_przedzialy) -> jeden
+
+cbind(rodzaj_klastra="rownoliczne_klastry", 
+      ile_poziomow_zmiennej_odpowiedzi=c(3, 5, 7, 10),
+      tabela_rownoliczne) -> dwa
+
+rbind(jeden, dwa) -> pom
+
+write.table(pom, "C:\\Users\\marsom\\Desktop\\wnioski\\sa.txt", col.names=TRUE, 
+            row.names=FALSE, quote=FALSE, sep="\t")
+
+
+
+
+
