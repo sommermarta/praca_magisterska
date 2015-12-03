d9daa54e6802a5e46a3db140a443a5820095a69a
Marta <sommerm@student.mini.pw.edu.pl>
2015-12-03 22:15:11 +0100

    babelki

---
+++ babelki.R
@@ -0,0 +1,54 @@
+
+posortuj <- function(wektor){
+
+     ile_zamian <- 0
+     for(j in length(wektor):2){
+          for(i in 1:(j-1)){
+               if(wektor[i]>wektor[i+1]){
+                    pamietaj <- wektor[i]
+                    wektor[i] <- wektor[i+1]
+                    wektor[i+1] <- pamietaj
+                    ile_zamian <- ile_zamian + 1
+               }     
+          }     
+     }
+     
+     ile_zamian
+}
+
+wsp <- function(wektor){
+     (posortuj(sort(wektor, decreasing=TRUE))-posortuj(wektor))/posortuj(sort(wektor, decreasing=TRUE))     
+}
+
+wektor <- c(5, 1, 1, 1, 2, 3, 4)
+wsp(wektor)
+
+wektor <- c(1, 1, 1, 2, 3, 5, 4)
+wsp(wektor)
+
+wektor <- c(5, 4, 3, 2, 1, 1, 1)
+wsp(wektor)
+
+wektor <- c(1, 1, 1, 2, 3, 4, 5)
+wsp(wektor)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
